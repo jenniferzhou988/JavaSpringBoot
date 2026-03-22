638097b5b45dc5309cd2c74dfd0197f80d63cb98
jenniferzhou988 <jennifer.zhou2005@gmail.com>
2026-03-22 11:34:07 -0400

    initial check in

---
+++ ShoppingChart/src/main/java/com/ShoppingCart/JZ/JzApplication.java
@@ -0,0 +1,13 @@
+package com.ShoppingCart.JZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JzApplication.class, args);
+	}
+
+}
